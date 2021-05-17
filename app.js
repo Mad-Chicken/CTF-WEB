cd3ae6068930a84e34d2d7f1306293076b0fd508
Mad-Chicken <legolemur@gmail.com>
2021-05-17 20:42:38 +0100

    foo

--- app.js
+++ app.js
@@ -5,18 +5,53 @@ const port = 3000
 const app = express();
 
 
-// setup static and middleware
-app.use(express.static('./public'));
+app.get('/', (req, res) => {
+	res.status(200).sendFile(path.resolve(__dirname, './public/html/index.html'));
+});
 
+
+// robots.txt
+app.get('/robots.txt', (req, res) => {
+        res.status(200).sendFile(path.resolve(__dirname, './public/html/robots.txt'));
+});
+
+
+
+// USERS //
+// login
+app.get('/login', (req, res) => {
+        res.status(200).sendFile(path.resolve(__dirname, './public/html/login/index.html'));
+	const username = req.params.username
+	console.log(username);
+});
+// regester
+app.get('/register', (req, res) => {
+        res.status(200).sendFile(path.resolve(__dirname, './public/html/register/index.html'));
+});
+
+
+
+// VIEW //
+// index
 app.get('/view', (req, res) => {
-//	res.status(200).send('Hello world');
-	res.status(200).sendFile(path.resolve(__dirname, './view/index.html'));
+        res.status(200).sendFile(path.resolve(__dirname, './public/html/view/index.html'));
+});
+// submit
+app.get('/view/submit', (req, res) => {
+        res.status(200).sendFile(path.resolve(__dirname, './public/html/view/submit/index.html'));
 });
 
-//404 page
+
+
+// 404 page
 app.all('*', (req,res)=>{
-	res.status(404).send('404 Not Found');
+	res.status(404)
+	res.sendFile(path.resolve(__dirname, './public/html/foo.html'));
 });
+
+
+
+
 
 app.listen(port, function(){
 	console.log('Server started on port :' + port);
