a880a0f0b0f96480cd44102908a33d59df335756
olivier <olivier@bodeswell.io>
2021-05-17 21:04:46 -0400

    bar

--- app.js
+++ app.js
@@ -11,68 +11,61 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/public/html/index.html');
 });
 
+
+//////// USERS ////////
+
+// login //
 app.get('/login', (req, res) => {
-	res.sendFile(__dirname + '/public/html/login/index.html');
+	res.status(200).sendFile(__dirname + '/public/html/login/index.html');
 });
 
+// login post //
 app.post('/login', (req, res) => {
-	// Insert Login Code Here
 	let username = req.body.username;
 	let password = req.body.password;
 	console.log(`Username: ${username} Password: ${password}`);
-	res.send(`Username: ${username} Password: ${password}`);
+	res.status(200).send(`Username: ${username} Password: ${password}`);
 });
 
+// regester //
+app.get('/register', (req, res) => {
+    res.status(200).sendFile(__dirname + '/public/html/register/index.html');
+});
+
+// regester post //
+app.post('/register', (req, res) => {
+	let username = req.body.username;
+	let password = req.body.password;
+	console.log(`Username: ${username} Password: ${password}`);
+	res.status(200).send(`Username: ${username} Password: ${password}`);
+});
+
+
+//////// VIEW ////////
+
+// index //
+app.get('/view', (req, res) => {
+    res.status(200).sendFile(__dirname + '/public/html/view/index.html');
+});
+
+// submit //
+app.get('/view/submit', (req, res) => {
+        res.status(200).sendFile(__dirname, './public/html/view/submit/index.html');
+});
+
+
+//////// robots.txt ////////
+app.get('/robots.txt', (req, res) => {
+    res.status(200).sendFile(__dirname, '/public/html/robots.txt');
+});
+
+//////// 404 ////////
+app.all('*', (req,res)=>{
+	res.status(404);
+	res.sendFile(__dirname + '/public/html/fourOfour.html');
+});
+
+//////// Listener ////////
 app.listen(port, function(){
 	console.log('Server started on port :' + port);
 });
-
-/*
-app.get('/', (req, res) => {
-	res.status(200).sendFile(path.resolve(__dirname, './public/html/index.html'));
-});
-
-
-// robots.txt
-app.get('/robots.txt', (req, res) => {
-    res.status(200).sendFile(path.resolve(__dirname, './public/html/robots.txt'));
-});
-
-
-
-// USERS //
-// login
-app.get('/login', (req, res) => {
-    res.status(200).sendFile(path.resolve(__dirname, './public/html/login/index.html'));
-});
-
-app.post('/view', (req, res) => {
-	console.log(req)
-	res.end()
-})
-
-// regester
-app.get('/register', (req, res) => {
-    res.status(200).sendFile(path.resolve(__dirname, './public/html/register/index.html'));
-});
-
-
-
-// VIEW //
-// index
-app.get('/view', (req, res) => {
-    res.status(200).sendFile(path.resolve(__dirname, './public/html/view/index.html'));
-});
-// submit
-app.get('/view/submit', (req, res) => {
-        res.status(200).sendFile(path.resolve(__dirname, './public/html/view/submit/index.html'));
-});
-
-
-
-// 404 page
-app.all('*', (req,res)=>{
-	res.status(404).send("<h1>Erro 404</h1>");
-	//res.sendFile(path.resolve(__dirname, './public/html/foo.html'));
-});
-*/
