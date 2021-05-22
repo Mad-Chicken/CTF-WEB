42b3dfd06e2b8dd92f05cae4f5f28771f681ff77
olivier <olivier@bodeswell.io>
2021-05-22 18:19:22 -0400

    More more more !

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var cookieParser = require('cookie-parser');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const e = require('express');
 
 const port = 3000
 const app = express();
@@ -19,7 +20,6 @@ app.use(session({
 }))
 
 app.get('/', (req, res) => {
-	//res.sendFile(__dirname + '/public/html/index.ejs');
 	var Name, URL
 	if (req.session.authenticated) {
 		Name = "Logout"
@@ -98,7 +98,7 @@ app.get('/logout', (req, res) => {
 app.get('/admin', (req, res) => {
 	if (req.session.authenticated) {
 		if (req.session.authenticatedAgent) {
-			res.status(200).sendFile(__dirname + '/private/index.html');
+			res.status(200).sendFile(__dirname + '/private/html/index.html');
 		} else {
 			res.status(200).sendFile(__dirname + '/public/html/admin/index.html');
 		}
@@ -152,9 +152,9 @@ app.get('/private/missions.css', (req, res) => {
 	if (req.session.authenticatedAgent) {
 		res.sendFile(__dirname + '/private/css/missions.css');
 	} else if (req.session.authenticated) {
-		res.status(403)
+		res.status(403);
 	} else {
-		res.status(401)
+		res.status(401);
 	}
 });
 
@@ -162,15 +162,80 @@ app.get('/private/missions.css', (req, res) => {
 app.get('/missions/mission_photo.jpg', (req, res) => {
 	if (req.session.authenticated) {
 		if (req.session.authenticatedAgent) {
-			//res.status(200).sendFile(__dirname + '/private/missions/mission_photo.jpg');
 			res.status(200).download(__dirname + '/private/images/mission_photo.jpg');
 		} else {
-			res.status(403).sendFile(__dirname + '/public/html/fourOfour.html');
+			res.status(403);
 		}
 	} else {
-		res.status(401).sendFile(__dirname + '/public/html/fourOfour.html');
+		res.status(401);
 	}
 });
+
+// exif location #2 //
+app.get('/missions/mission_location_photo.png', (req, res) => {
+	if (req.session.authenticated) {
+		if (req.session.authenticatedAgent) {
+			res.status(200).download(__dirname + '/private/images/mission_location_photo.png');
+		} else {
+			res.status(403);
+		}
+	} else {
+		res.status(401);
+	}
+});
+
+// browser //
+app.get('/missions/browser/', (req, res) => {
+	if (req.session.authenticatedAgent) {
+		if (req.headers['user-agent'] == "AgencySecureBrowser") {
+			res.status(200);
+			res.render(__dirname + '/private/html/missions/browser/index.ejs', {
+				data: "Latest version of AgencySecureBrowser required (4.2.0)"
+			});
+		} else if (req.headers['user-agent'] == "AgencySecureBrowser/4.2.0") {
+			res.status(200);
+			res.render(__dirname + '/private/html/missions/browser/index.ejs', {
+				data: "flag(bR0w$er_bR0w$_bR0w$er)"
+			});
+		} else {
+			res.status(200);
+			res.render(__dirname + '/private/html/missions/browser/index.ejs', {
+				data: "Use of 'AgencySecureBrowser' browser required"
+			});
+		}
+	} else if (req.session.authenticated) {
+		res.status(403);
+	} else {
+		res.status(401);
+	}
+});
+
+// Captured website //
+app.get('/missions/capturedWebsite/', (req, res) => {
+	if (req.session.authenticatedAgent) {
+		res.cookie('admin', 'false', {maxAge: 60000});
+		//res.sendFile(__dirname + '/private/html/missions/capturedWebsite/index.html');
+		res.render(__dirname + '/private/html/missions/capturedWebsite/index.ejs', {
+			data: ""
+		});
+	} else if (req.session.authenticated) {
+		res.status(403);
+	} else {
+		res.status(401);
+	}
+});
+
+app.post('/missions/capturedWebsite/', (req, res) => {
+	res.status(200)
+	if (req.cookies['admin'] == "true") {
+		res.render(__dirname + '/private/html/missions/capturedWebsite/index.ejs', {
+			data: "flag(cH@nge_c00k!e_2_e@t_?)"
+		});
+	} else {
+		res.redirect("/missions/capturedWebsite/");
+	}
+});
+
 
 
 //////// MESSAGES ////////
